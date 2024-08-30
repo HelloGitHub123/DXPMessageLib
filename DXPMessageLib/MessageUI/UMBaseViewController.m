59020647fa5739260d49b291a59d117748efba8e
李标 <li.biao3@iwhalecloud.com>
2024-08-30 16:36:20 +0800

    fix

--- DXPMessageLib/MessageUI/UMBaseViewController.m
+++ DXPMessageLib/MessageUI/UMBaseViewController.m
@@ -53,7 +53,10 @@
     if (viewcontrolls.count==1) {
         self.navigationItem.leftBarButtonItem = nil;
         [self.navigationItem setHidesBackButton:YES];
-    }
+		[[NSNotificationCenter defaultCenter] postNotificationName:@"IsHiddenTabbarView" object:@{@"count":@(0)}];
+	} else if (viewcontrolls.count >= 1) {
+		[[NSNotificationCenter defaultCenter] postNotificationName:@"IsHiddenTabbarView" object:@{@"count":@(1)}];
+	}
 }
 
 - (void)setNavTitleStr:(NSString *)navTitleStr {
